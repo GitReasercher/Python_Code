ba501fa5d73e0815ca12a99afc38940fd51b9b30
GitReasercher <asadabd.567@gmail.com>
2026-02-26 18:33:08 +1000

    Update code

--- Dunder-методы.py
+++ Dunder-методы.py
@@ -41,3 +41,57 @@ class BankAccount:
 
 my_account = BankAccount(5007.45)
 print(my_account)
+
+
+
+class Multiplier:
+     def __init__(self, factor):
+         self.factor = factor
+
+     def __call__(self, value):
+         return value * self.factor
+
+# Создаем экземпляр (объект)
+triple = Multiplier(3)
+
+# Используем объект как функцию
+print(triple(10))  # Выведет 30
+
+
+class Math:
+
+    def mul(self, x, y):
+        self.x = x
+        self.y = y
+        return x * y
+
+    def __call__(self, number):
+        return self.x * self.y + number
+
+counting = Math()
+
+counting.mul(5, 6)
+print(counting(1))
+
+
+#3
+
+class Counter:
+
+    def addition(self, number):
+        self.number = number
+        return number
+
+    def __call__(self):
+        return self.number + 1
+
+Addition = Counter()
+Addition.addition(5)
+print(Addition())
+
+
+
+class Math:
+    def multiply(self, number):
+        self.number = number
+        return number * self.number
