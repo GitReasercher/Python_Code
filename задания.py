ac7219bef088168ed150055b1dcc8e666818d96c
GitReasercher <asadabd.567@gmail.com>
2026-02-28 11:01:16 +1000

    Update code

---
+++ задания.py
@@ -0,0 +1,23 @@
+import sys as s
+
+def new_user(password):
+
+    has_letter = False
+
+    has_digit = False
+
+    for i in password:
+        if i.isdigit():
+            has_digit = True
+            break
+
+    for i in password:
+        if i.isalpha:
+            has_letter = True
+            break
+
+    password_long = 8
+    if password < password_long:
+        s.exit('В пароле должно быть минимум 8 символов, включая: буквы, цифры')
+
+print(new_user(5678643))
