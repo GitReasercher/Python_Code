784e2d13587a6262c82603355b44ffbd0fb63d26
GitReasercher <asadabd.567@gmail.com>
2026-03-12 19:11:47 +1000

    Update code

---
+++ Информатика.py
@@ -0,0 +1,12 @@
+import math
+
+def summa(a, b, c):
+    return sum([a, b, c])
+
+def mul(a, b, c):
+    return math.prod([a, b, c])
+
+def div(a, b, c):
+    return a / b /c
+
+print(summa(1, 1, 9))
